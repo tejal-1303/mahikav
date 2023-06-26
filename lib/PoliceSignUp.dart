d4ca36b3f332cb6cba3c00291be8bcc13d7ad464
tejal-1303 <79006362+tejal-1303@users.noreply.github.com>
2023-06-26 17:11:17 +0530

    added the calling police and recording service in the needed pages

--- lib/PoliceSignUp.dart
+++ lib/PoliceSignUp.dart
@@ -1,203 +1,70 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mahikav/components/buttons/dropdown_text_field.dart';
 import 'package:mahikav/components/text_form_field.dart';
-import 'package:mahikav/home_page.dart';
 
 import 'components/buttons/filled_buttons.dart';
-import 'constants.dart';
 
 class PoliceSignUp extends StatefulWidget {
   const PoliceSignUp({Key? key}) : super(key: key);
 
   @override
-  State<PoliceSignUp> createState() => _StudentSignUp();
+  State<PoliceSignUp> createState() => _PoliceSignUp();
 }
 
-class _StudentSignUp extends State<PoliceSignUp> {
+class _PoliceSignUp extends State<PoliceSignUp> {
   final emailCtrl = TextEditingController();
   final passCtrl = TextEditingController();
-  final policeIdCtrl = TextEditingController();
-  final policePostCtrl = TextEditingController();
-  final idProofCtrl = TextEditingController();
-  final confPassCtrl = TextEditingController();
-  final nameCtrl = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-  String? errorText, postError;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: ListView(
+        child: Column(
           children: [
             Form(
-              key: _formKey,
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Column(
+
                   crossAxisAlignment: CrossAxisAlignment.stretch,
+
                   children: [
                     CustomTextFormField(
                       label: 'Email ID',
                       controller: emailCtrl,
                       hint: 'abc@example.com',
-                      validator: (val) {
-                        if (val == null || val.isEmpty) {
-                          return 'Field is Required';
-                        }
-                        return errorText;
-                      },
                     ),
-                    SizedBox(height: 10),
+                    SizedBox(
+                      height: 10,
+                    ),
                     CustomTextFormField(
                       label: 'Police ID',
-                      controller: policeIdCtrl,
-                      validator: (val) {
-                        if (val == null || val.isEmpty) {
-                          return 'Field is Required';
-                        }
-                        return null;
-                      },
+                      controller: emailCtrl,
+                      hint: 'abc@example.com',
                     ),
-                    SizedBox(height: 10),
-                    CustomDropDownField(
-                      label: 'Post',
-                      controller: policePostCtrl,
-                      listItems: [
-                        'Constable',
-                        'Head Constable',
-                        'Assistant Sub-Inspector',
-                        'Sub-Inspector',
-                        'Inspector',
-                        'Deputy Superintendant of Police',
-                        'Additional Superintendent of Police',
-                        'Superintendent of Police',
-                        'Senior Superintendent of Police',
-                        'Deputy Inspector General of Police',
-                        'Inspector-General of Police',
-                        'Director-General of Police',
-                      ], errorText: postError,
-                    ),
-                    CustomTextFormField(
-                      label: 'Name',
-                      controller: nameCtrl,
-                      validator: (val) {
-                        if (val == null || val.isEmpty) {
-                          return 'Field is Required';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 10),
-                    CustomTextFormField(
-                      label: 'Aadhar Card No.',
-                      controller: idProofCtrl,
+                    SizedBox(
+                      height: 10,
+                    ),CustomTextFormField(
+                      label: 'ID Proof',
+                      controller: emailCtrl,
                       hint: 'Eg.Aadhar Card, Pan Card etc... ',
-                      validator: (val) {
-                        if (val == null || val.isEmpty) {
-                          return 'Field is Required';
-                        }
-                        return null;
-                      },
                     ),
-                    SizedBox(height: 10),
+                    SizedBox(
+                      height: 10,
+                    ),
                     CustomTextFormField(
-                      label: 'Password',
-                      controller: passCtrl,
-                      isPassword: true,
-                      validator: (val) {
-                        if (val == null || val.isEmpty) {
-                          return 'Field is Required';
-                        }
-                        return null;
-                      },
+                        label: 'Password', controller: passCtrl),
+                    SizedBox(
+                      height: 10,
                     ),
-                    SizedBox(height: 10),
                     CustomTextFormField(
-                      label: 'Confirm Password',
-                      controller: confPassCtrl,
-                      isPassword: true,
-                      validator: (val) {
-                        if (val == null || val.isEmpty) {
-                          return 'Field is Required';
-                        } else if (val != passCtrl.text) {
-                          return 'Password Doesn\'t Match';
-                        }
-                        return null;
-                      },
+                        label: 'Confirm Password', controller: passCtrl),
+                    SizedBox(
+                      height: 20,
                     ),
-                    SizedBox(height: 20),
                     CustomFilledButton(
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          if (policePostCtrl.text.isEmpty) {
-                            postError = 'Add Post';
-                            setState(() {
-
-                            });
-                            return;
-                          }
-                          postError = null;
-                          setState(() {
-
-                          });
-                          try {
-                            await FirebaseAuth.instance
-                                .createUserWithEmailAndPassword(
-                              email: emailCtrl.text,
-                              password: passCtrl.text,
-                            );
-                          } on FirebaseAuthException catch (e) {
-                            switch (e.code) {
-                              case 'email-already-in-use':
-                                {
-                                  errorText = 'Email already in use';
-                                  setState(() {});
-                                  break;
-                                }
-                              case 'invalid-email':
-                                {
-                                  errorText = 'Invalid Email';
-                                  setState(() {});
-                                  break;
-                                }
-                              default:
-                                {
-                                  errorText = 'Something Went Wrong';
-                                  setState(() {});
-                                  break;
-                                }
-                            }
-                            _formKey.currentState!.validate();
-                            errorText = null;
-                            setState(() {});
-                            return;
-                          }
-                          await FirebaseFirestore.instance
-                              .collection('users')
-                              .doc(auth.currentUser!.uid)
-                              .set({
-                            'name': nameCtrl.text,
-                            'email': emailCtrl.text,
-                            'category': 'Police',
-                            'IDProof': idProofCtrl.text,
-                            'city': 'Gwalior',
-                            'state': 'Madhya Pradesh',
-                            'policeID': policeIdCtrl.text,
-                            'post': policePostCtrl.text,
-                            'isVerifiedUser': null,
-                          });
-                          Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(builder: (_) => HomePage()),
-                                (route) => false,
-                          );
-                          // TODO: Add Navigator to HomePage
-                        }
-                      },
+                      onPressed: () {},
                       label: 'Sign Up',
                     ),
                   ],
--- lib/login_with_phone.dart
+++ lib/login_with_phone.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:mahikav/components/buttons/filled_buttons.dart';
 import 'package:mahikav/constants.dart';
 
+import 'components/custom_icon_icons.dart';
 import 'components/text_form_field.dart';
 
 class LoginWithPhone extends StatefulWidget {
@@ -22,6 +24,34 @@ class _LoginWithPhoneState extends State<LoginWithPhone> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton:
+      Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.red.shade900,
+            child: Icon(
+              CustomIcon.mic,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            child: Icon(
+              Icons.call,
+              color: Colors.white,
+            ),
+            onPressed: () async {
+              await FlutterPhoneDirectCaller.callNumber(
+                  '+917021051913');
+            },
+          ),
+        ],
+      ),
       body: Form(
         key: _formKey,
         child: Column(
